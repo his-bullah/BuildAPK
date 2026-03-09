ccc1451e4da3250a9678815f4fa0bfa7f65eb38f
his-bullah <his.bullah.pvt.060@gmail.com>
2026-03-10 01:58:12 +0530

    Update service.py

--- service.py
+++ service.py
@@ -1,12 +1,8 @@
 import time
-from jnius import autoclass
 
-# Idhu unga service-oda main loop
-def run_service():
-    while True:
-        print("Ennoda Foreground Service run aagitu irukku!")
-        # 5 seconds-kku oru thadava print aagum
-        time.sleep(5) 
+c = 1
 
-if __name__ == '__main__':
-    run_service()
+while True:
+    print(f"Running... {c}")
+    time.sleep(5)
+    c += 1
