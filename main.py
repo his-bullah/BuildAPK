f034a96aaad449d8ea27a5e0e96b0cf89faa6d01
his-bullah <his.bullah.pvt.060@gmail.com>
2026-03-19 01:41:58 +0530

    Refactor service start and icon hide methods

--- main.py
+++ main.py
@@ -6,53 +6,40 @@ from kivy.clock import Clock
 class ForegroundApp(App):
     def build(self):
         layout = BoxLayout(orientation='vertical', padding=50, spacing=20)
-
         start_btn = Button(text="Start Foreground Service", font_size=20)
         start_btn.bind(on_press=self.start_service)
         layout.add_widget(start_btn)
-
         self.hide_btn = Button(text="Hide App Icon", font_size=18)
         self.hide_btn.bind(on_press=self.hide_icon_delay)
         layout.add_widget(self.hide_btn)
-
         return layout
 
-    def start_service(self, instance):
+    def start_service(self,instance):
         try:
             from jnius import autoclass
             mActivity = autoclass('org.kivy.android.PythonActivity').mActivity
-            ServiceClass = autoclass('org.test.foregroundapp.ServiceMyservice')
-            ServiceClass.start(mActivity, "Foreground service started from app!")
+            ServiceClass = autoclass(f'{mActivity.getPackageName()}.ServiceMyservice')
+            ServiceClass.start(mActivity, "Foreground service started!")
             instance.text = "Service Running..."
         except Exception as e:
-            instance.text = f"Error: {e}"
+            instance.text = f"Starting Error: {e}"
 
-    def hide_icon_delay(self, instance):
-        instance.text = "Icon hiding in 5s..."
-        Clock.schedule_once(self.disable_app, 5)
+    def hide_icon_delay(self,instance):
+        instance.text = "Icon Hiding in 5s..."
+        Clock.schedule_once(self.disable_app,5)
 
-    def disable_app(self, dt):
+    def disable_app(self,dt):
         try:
             from jnius import autoclass
-
             PythonActivity = autoclass('org.kivy.android.PythonActivity')
             PackageManager = autoclass('android.content.pm.PackageManager')
-
             activity = PythonActivity.mActivity
             pm = activity.getPackageManager()
             package_name = activity.getPackageName()
-
-            # 🔥 MAIN TRICK (works in buildozer)
-            pm.setApplicationEnabledSetting(
-                package_name,
-                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-                PackageManager.DONT_KILL_APP
-            )
-
+            pm.setApplicationEnabledSetting(package_name,PackageManager.COMPONENT_ENABLED_STATE_DISABLED,PackageManager.DONT_KILL_APP)
             print("App icon hidden")
-
-        except Exception as e:
-            print("Error:", e)
+        except Exception as error:
+            print("Error:", error)
 
 
 ForegroundApp().run()
